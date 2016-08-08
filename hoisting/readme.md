eda430aff1d3595a655f788ad1a3985bd22f2fcd
Miller Gonzalez <miller.gonzalez@salesforce.com>
2016-08-08 10:36:05 -0500

    Hoisting basic var explanation

---
+++ hoisting/readme.md
@@ -0,0 +1,5 @@
+Hoisting
+========
+
+
+In ES5 JavaScript moves the code to the top of the scope this means all var declarations go to the top of the funcion
---
+++ hoisting/var_example.js
@@ -0,0 +1,19 @@
+function loadUserProfiles(userNames){
+    if(userNames.length > 3){
+        var slowMessage = "This could take a while";
+        displayMessage(slowMessage);
+    } else {
+        var fastMessage = "Loading users";
+        displayMessage(fastMessage);
+    }
+
+    console.log(fastMessage);
+}
+
+function displayMessage(message){
+    //do something ex: show spinner...
+}
+
+//what will happen when case userNames.length > 3
+loadUserProfiles(['Jhon', 'Carl', 'Pepe', 'Monty']); // => will show undefined for fastMessage
+
